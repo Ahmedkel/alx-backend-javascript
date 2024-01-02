b58192fd0b0157299b86b3ab7298f80bb3e5c24b
Ahmedkel <ahmedkel@gmail.com>
2024-01-02 18:51:37 +0100

    For...of Loops

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const ary = [];
+    for (const idx of array) {
+    var value = array[idx];
+    ary.push(appendString + value);
+  }
+
+  return array;
+}
