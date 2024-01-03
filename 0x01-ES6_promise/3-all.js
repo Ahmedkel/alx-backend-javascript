3feb01f4247bc97c6003304a39811fd1b4482c76
Ahmedkel <ahmedkel@gmail.com>
2024-01-03 16:21:48 +0100

    Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,14 +2,25 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then(([photoResponse, userResponse]) => {
-      console.log(
-        photoResponse.body,
-        userResponse.firstName,
-        userResponse.lastName,
-      );
+    .then((values) => {
+      console.log(values[0].body, values[1].firstName, values[1].lastName);
     })
-    .catch(() => {
-      console.log('Signup system offline');
-    });
+    .catch(() => console.log('Signup system offline'));
 }
+
+// alternative solution
+// import { uploadPhoto, createUser } from './utils';
+
+// export default function handleProfileSignup() {
+//   Promise.all([uploadPhoto(), createUser()])
+//     .then(([photoResponse, userResponse]) => {
+//       console.log(
+//         photoResponse.body,
+//         userResponse.firstName,
+//         userResponse.lastName,
+//       );
+//     })
+//     .catch(() => {
+//       console.log('Signup system offline');
+//     });
+// }
