2a5c529ddc351f0f3dd6570a049d20a18cc0475a
Ahmedkel <ahmedkel@gmail.com>
2024-01-08 20:33:29 +0100

    Cleanset

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,8 @@
 export default function cleanSet(set, str) {
-  if (str === undefined || str === '') return '';
-  return [...set]
-    .filter((val) => (val ? val.startsWith(str) : ''))
-    .map((val) => (val ? val.slice(str.length) : ''))
-    .join('-');
+  if (str === '') return '';
+  const newSet = [];
+  set.forEach((item) => {
+    if (item.includes(str)) newSet.push(item.replace(str, ''));
+  });
+  return newSet.join('-');
 }
